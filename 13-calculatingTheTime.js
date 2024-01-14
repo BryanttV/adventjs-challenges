eeab3c210c015f66353ab8ddc1af1b6265018b5e
BryanttV <bryanttandrei2015@gmail.com>
2024-01-14 08:35:13 -0500

    feat: improve challenge #13 (2023)

--- 13-calculatingTheTime.js
+++ 13-calculatingTheTime.js
@@ -1,46 +1,23 @@
 function calculateTime(deliveries) {
   const maxSeconds = 7 * 3600;
-  const len = deliveries.length;
-  const times = deliveries.map(function (deliver) {
-    return deliver.split(":").map(Number);
-  });
-
-  let hours = 0;
-  let minutes = 0;
-  let seconds = 0;
-
-  for (let i = 0; i < len; i++) {
-    hours += times[i][0];
-    minutes += times[i][1];
-    seconds += times[i][2];
-  }
-
-  const convertToSeconds = (hours, minutes, seconds) => {
-    let totalSeconds = seconds;
-    totalSeconds += minutes * 60;
-    totalSeconds += hours * 3600;
-    return totalSeconds;
-  };
-
-  let totalSeconds = convertToSeconds(hours, minutes, seconds);
+  const times = deliveries
+    .map(function (deliver) {
+      return deliver.split(":").map(Number);
+    })
+    .reduce((acc, time) => {
+      return acc + time[0] * 3600 + time[1] * 60 + time[2];
+    }, 0);
 
   const convertToTime = (seconds) => {
-    const isNegative = seconds < 0 ? true : false;
-    if (isNegative) {
-      seconds *= -1;
-    }
-    const hours = Math.floor(seconds / 3600).toString();
-    const minutes = Math.floor((seconds % 3600) / 60).toString();
-    const remainingSeconds = (seconds % 60).toString();
-    return `${isNegative ? "" : "-"}${hours.padStart(
-      2,
-      "0"
-    )}:${minutes.padStart(2, "0")}:${remainingSeconds.padStart(2, "0")}`;
+    const hours = `${Math.floor(seconds / 3600)}`.padStart(2, 0);
+    const minutes = `${Math.floor((seconds % 3600) / 60)}`.padStart(2, 0);
+    const remainingSeconds = `${seconds % 60}`.padStart(2, 0);
+    return `${hours}:${minutes}:${remainingSeconds}`;
   };
 
-  const difference = maxSeconds - totalSeconds;
-
-  return difference == 0 ? "00:00:00" : convertToTime(difference);
+  const difference = maxSeconds - times;
+  const finalTime = convertToTime(Math.abs(difference));
+  return difference <= 0 ? finalTime : `-${finalTime}`;
 }
 
 const time = calculateTime(["00:10:00", "01:00:00", "03:30:00"]);
