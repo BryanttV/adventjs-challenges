3117afa522ca0ba73dada6ec0fba02241fc7a58a
Bryann Valderrama <64033729+BryanttV@users.noreply.github.com>
2025-03-15 16:25:02 +0000

    feat(2024): add challenge #11 in python

---
+++ 2024/python/11_filenames_encoded.py
@@ -0,0 +1,19 @@
+import re
+
+def decode_filename(filename: str) -> str:
+  """Decode filename.
+
+  Args:
+      filename (str): The filename
+
+  Returns:
+      str: The decoded filename
+  """
+  return re.search(r"(\d+)_([a-zA-Z_-]+.\w+)", filename).group(2)
+
+
+print(decode_filename('2023122512345678_sleighDesign.png.grinchwa')) # "sleighDesign.png"
+
+print(decode_filename('42_chimney_dimensions.pdf.hack2023')) # "chimney_dimensions.pdf"
+
+print(decode_filename('987654321_elf-roster.csv.tempfile')) # "elf-roster.csv"
