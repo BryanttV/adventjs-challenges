69fcd918d42ef7d88a40516ab92015671e530f49
Bryann Valderrama <bryanttandrei2015@gmail.com>
2025-03-22 22:33:39 -0500

    feat(2024): add challenge #12 in python and javascript

---
+++ 2024/python/12_how_much_does_the_tree_cost.py
@@ -0,0 +1,40 @@
+"""Challenge #12: 💵 How Much Does The Tree Cost?"""
+
+
+def calculate_price(ornaments: str) -> int | None:
+    """Calculate the total value of a Christmas tree based on its ornaments.
+
+    Args:
+        ornaments (str): A string representing the ornaments on the tree.
+
+    Returns:
+        int: The calculated tree value or None if an invalid character is present.
+    """
+    values = {"*": 1, "o": 5, "^": 10, "#": 50, "@": 100}
+    total = 0
+    prev_value = 0
+
+    for ornament in reversed(ornaments):
+        if ornament not in values:
+            return None
+        value = values[ornament]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total
+
+
+print(calculate_price("***"))  # 3 (1 + 1 + 1)
+print(calculate_price("*o"))  # 4 (5 - 1)
+print(calculate_price("o*"))  # 6 (5 + 1)
+print(calculate_price("*o*"))  # 5 (-1 + 5 + 1)
+print(calculate_price("**o*"))  # 6 (1 - 1 + 5 + 1)
+print(calculate_price("o***"))  # 8 (5 + 3)
+print(calculate_price("*o@"))  # 94 (-5 - 1 + 100)
+print(calculate_price("*#"))  # 49 (-1 + 50)
+print(calculate_price("@@@"))  # 300 (100 + 100 + 100)
+print(calculate_price("#@"))  # 50 (-50 + 100)
+print(calculate_price("#@Z"))  # undefined (Z is unknown)
